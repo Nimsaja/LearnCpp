6e4f731ef4d2d6abe9a54faf3776aadfbdd5df29
Nimsaja <jasmin.roeper@nexgo.de>
2018-11-23 15:52:25 +0100

    1.10a Header Guards

---
+++ 110_HeaderGuard/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "geometry.h"
+#include "math.h"
+
+int main() {
+    std::cout << "sides of a square: " << getSquareSides() << std::endl;
+
+    return 0;
+}
---
+++ 110_HeaderGuard/math.h
@@ -0,0 +1,11 @@
+//this prevents getSquareSides to be included in main twice
+
+#ifndef MATH_H
+#define MATH_H
+
+int getSquareSides()
+{
+    return 4;
+}
+
+#endif
