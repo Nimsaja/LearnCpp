23a1f1e8cc1e5e4d2eb12db08706871b1b1afb87
Nimsaja <jasmin.roeper@nexgo.de>
2018-11-23 12:00:08 +0100

    1.9 Header Files

---
+++ 19_HeaderFiles/add.h
@@ -0,0 +1,11 @@
+/*This is start of the header guard (will prevent a given heade file from being #included more than once
+ * in the same file). ADD_H can be any unique name. By convention, we use the name of the header file.
+ */
+#ifndef ADD_H
+#define ADD_H
+
+//This is the content of the .h file, which is where the declarations go
+int add(int x, int y); //function prototype
+
+//This is the end of the header guard
+#endif
--- README.md
+++ README.md
@@ -5,3 +5,7 @@ g++ -pedantic-errors -Wall -Weffc++ -Wextra -Wsign-conversion -o HelloWorld Hell
 
 -pedantic-errors : when code is not standard c++
 -Wall -Weffc++ -Wextra -Wsign-conversion : increase warning levels
+
+Include other "search directories" for header files
+
+g++ -o main -I /source/includes main.cpp
