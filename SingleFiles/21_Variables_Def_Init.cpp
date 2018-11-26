1dc64159bf260790825e20185728105119e331f4
Nimsaja <jasmin.roeper@nexgo.de>
2018-11-26 14:19:44 +0100

    2.1 Variable assignment and sizes

---
+++ SingleFiles/21_Variables_Def_Init.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+void sizeOfTypes()
+{
+    std::cout << "bool:\t\t" << sizeof(bool) << " bytes" << std::endl;
+    std::cout << "char:\t\t" << sizeof(char) << " bytes" << std::endl;
+    std::cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes" << std::endl;
+    std::cout << "char16_t:\t" << sizeof(char16_t) << " bytes" << std::endl; // C++11, may not be supported by your compiler
+    std::cout << "char32_t:\t" << sizeof(char32_t) << " bytes" << std::endl; // C++11, may not be supported by your compiler
+    std::cout << "short:\t\t" << sizeof(short) << " bytes" << std::endl;
+    std::cout << "int:\t\t" << sizeof(int) << " bytes" << std::endl;
+    std::cout << "long:\t\t" << sizeof(long) << " bytes" << std::endl;
+    std::cout << "long long:\t" << sizeof(long long) << " bytes" << std::endl; // C++11, may not be supported by your compiler
+    std::cout << "float:\t\t" << sizeof(float) << " bytes" << std::endl;
+    std::cout << "double:\t\t" << sizeof(double) << " bytes" << std::endl;
+    std::cout << "long double:\t" << sizeof(long double) << " bytes" << std::endl;
+}
+
+int main()
+{
+    int value_c = 5; // copy initialization
+
+    int value_d(5); // direct initialization
+
+    int value_u{5}; // uniform initialization -> prefer this!!
+
+    value_u = 17; // copy assignment
+
+    std::cout << value_c << std::endl;
+    std::cout << value_d << std::endl;
+    std::cout << value_u << std::endl;
+
+    sizeOfTypes();
+
+    return 0;
+}
